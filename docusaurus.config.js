972129d3f34f46774d6edd9ac1c8eaa11bafdcd2
mattdobbin <mattdobbin10@gmail.com>
2026-06-26 19:00:35 -0400

    Mostly front page changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,37 +114,41 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Community',
+            title: 'Contact',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                html: '<span>📧 mattdobbin10@gmail.com</span>',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                html: '<span>📞 416-577-2683</span>',
               },
               {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                html: '<span>📍 Ottawa , ON</span>',
+              },
+              {
+                html: '<a href="https://www.linkedin.com/in/mattdobbin10/" target="_blank" rel="noopener noreferrer" style="color:white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="vertical-align:middle;margin-right:6px"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.225 0z"/></svg>LinkedIn</a>',
               },
             ],
           },
           {
-            title: 'More',
+            title: 'Portfolio',
             items: [
+              {
+                label: 'Doc Samples',
+                to: '/docs',
+              },
               {
                 label: 'AI Workflows',
                 to: '/ai-workflows',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Resume',
+                href: 'https://mattdobbin.github.io/docusaurus-site/files/Matt-Dobbin-Resume.pdf',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        //copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
